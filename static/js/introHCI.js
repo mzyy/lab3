63c5faf72f6588f57eced39607561c3f08a3feac
mzyy <47995049+mzyy@users.noreply.github.com>
2021-01-21 11:28:09 -0800

    Update introHCI.js

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -12,6 +12,13 @@ function initializePage() {
 	$("#testjs").click(function(e) {
 		$('.jumbotron h1').text("Javascript is connected");
 	});
+function projectClick(e) { 
+    // prevent the page from reloading      
+    e.preventDefault();
+    // In an event handler, $(this) refers to      
+    // the object that triggered the event      
+    $(this).css("background-color", "#7fff00");
+}
 
 	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
